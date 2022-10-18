b281f7e393d81b538b45e298819f782980177e15
W.O.M.P <56969978+StijnJacobsPXL@users.noreply.github.com>
2022-10-18 13:28:38 +0000

    minor progress?

--- cmt_catches.js
+++ cmt_catches.js
@@ -54,72 +54,96 @@ async function listMiceOfCurrentLocation(){
 )
 }
 
-function render(){
-    const existing = document.querySelector("#cmt_crowns");
-    if (existing){
-        existing.remove();
-    }
-
-    const rawData = localStorage.getItem("CMT_catchstats");
-    if(rawData){
-        const data = JSON.parse(rawData);
-    }
-
-    // popup styling
-    const mainDiv = document.createElement("div");
-    mainDiv.id = "cmt_crowns"
-    mainDiv.style.backgroundColor = "#F5F5F5";
-    mainDiv.style.position = "fixed";
-    mainDiv.style.zIndex = "42";
-    mainDiv.style.left = "5px";
-    mainDiv.style.top = "5px";
-    mainDiv.style.border = "solid 3px #696969";
-    mainDiv.style.borderRadius = "20px";
-    mainDiv.style.padding = "10px";
-    mainDiv.style.textAlign = "center";
-
-    // Top div styling (close button, title, drag instructions)
-    const topDiv = document.createElement("div");
-
-    const titleSpan = document.createElement("span");
-    titleSpan.style.fontWeight = "bold";
-    titleSpan.style.fontSize = "18px";
-    titleSpan.style.textDecoration = "underline";
-    titleSpan.style.paddingLeft = "20px";
-    titleSpan.innerText = "Catch Stats";
-    const dragSpan = document.createElement("span");
-    dragSpan.innerText = "(Drag title to reposition this popup)";
-
-    const closeButton = document.createElement("button");
-    closeButton.style.float = "right";
-    closeButton.style.fontSize = "8px";
-    closeButton.textContent = "x";
-    closeButton.onclick = function () {
-      document.body.removeChild(mainDiv);
-    };
-
-    topDiv.appendChild(closeButton);
-    topDiv.appendChild(titleSpan);
-    topDiv.appendChild(document.createElement("br"));
-    topDiv.appendChild(dragSpan);
-    
-
+function addLinkToUI (){
+    const target = document.querySelector(".mousehuntHud-gameInfo");
+    const div = document.createElement("div");
+    div.id = "cmt-locationcatches-link";
+    const link = document.createElement("a");
+    link.innerText = "[Mice caught]";
+    link.addEventListener("click", function () {
+      const existing = document.querySelector("#cmt_micecaught_modal");
+      if (existing) existing.remove();
+      else uponButtonClick();
+    });
+    div.appendChild(document.createElement("br"));
+    div.appendChild(button);
+    target.appendChild(div);
 }
 
-// Inject initial button/link into UI
-function injectUI() {
-    document.querySelectorAll("#crown_progress_button").forEach(el => el.remove());
-    const target = document.querySelector(".mousehuntHud-gameInfo");
-    if (target) {
-    const link = document.createElement("a");
-    link.id = "crown_progress_button";
-    link.innerText = "[Crown progress]";
-    link.addEventListener("click", function () {
-        const existing = document.querySelector("#cmt_crowns");
-        if (existing) existing.remove();
-        else render();
-        return false; // Prevent default link clicked behavior
-    });
-    target.prepend(link);
-    }
-}})();
+function uponButtonClick(){
+    console.log("smlj");
+}
+// Not yet specified or used jank
+
+// function render(){
+//     const existing = document.querySelector("#cmt_crowns");
+//     if (existing){
+//         existing.remove();
+//     }
+
+//     const rawData = localStorage.getItem("CMT_catchstats");
+//     if(rawData){
+//         const data = JSON.parse(rawData);
+//     }
+
+//     // popup styling
+//     const mainDiv = document.createElement("div");
+//     mainDiv.id = "cmt_crowns"
+//     mainDiv.style.backgroundColor = "#F5F5F5";
+//     mainDiv.style.position = "fixed";
+//     mainDiv.style.zIndex = "42";
+//     mainDiv.style.left = "5px";
+//     mainDiv.style.top = "5px";
+//     mainDiv.style.border = "solid 3px #696969";
+//     mainDiv.style.borderRadius = "20px";
+//     mainDiv.style.padding = "10px";
+//     mainDiv.style.textAlign = "center";
+
+//     // Top div styling (close button, title, drag instructions)
+//     const topDiv = document.createElement("div");
+
+//     const titleSpan = document.createElement("span");
+//     titleSpan.style.fontWeight = "bold";
+//     titleSpan.style.fontSize = "18px";
+//     titleSpan.style.textDecoration = "underline";
+//     titleSpan.style.paddingLeft = "20px";
+//     titleSpan.innerText = "Catch Stats";
+//     const dragSpan = document.createElement("span");
+//     dragSpan.innerText = "(Drag title to reposition this popup)";
+
+//     const closeButton = document.createElement("button");
+//     closeButton.style.float = "right";
+//     closeButton.style.fontSize = "8px";
+//     closeButton.textContent = "x";
+//     closeButton.onclick = function () {
+//       document.body.removeChild(mainDiv);
+//     };
+
+//     topDiv.appendChild(closeButton);
+//     topDiv.appendChild(titleSpan);
+//     topDiv.appendChild(document.createElement("br"));
+//     topDiv.appendChild(dragSpan);
+    
+
+// }
+
+// // Inject initial button/link into UI
+// function injectUI() {
+//     document.querySelectorAll("#crown_progress_button").forEach(el => el.remove());
+//     const target = document.querySelector(".mousehuntHud-gameInfo");
+//     if (target) {
+//     const link = document.createElement("a");
+//     link.id = "crown_progress_button";
+//     link.innerText = "[Crown progress]";
+//     link.addEventListener("click", function () {
+//         const existing = document.querySelector("#cmt_crowns");
+//         if (existing) existing.remove();
+//         else render();
+//         return false; // Prevent default link clicked behavior
+//     });
+//     target.prepend(link);
+//     }
+// }
+
+
+})();
